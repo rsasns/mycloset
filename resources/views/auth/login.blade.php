f71f5be9d68306b0bfdc7d266a861748eeb4559d
ran.sakata <sakataran@outlook.jp>
2021-07-01 11:41:02 +0000

    fixed top

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,17 +24,17 @@
                     <a href="{{ url('login/facebook')}}" class="link text-dark font-weight-bold"><i class="fab fa-facebook mr-3"></i>Facebookでログイン</a>
                 </div>
                 <div class="col-md-12 mt-2">
-                    <a href="{{ route('password.request') }}" class="link text-dark small">パスワードを忘れた場合<a>
+                    <a href="{{ route('password.request') }}" class="link text-dark small">パスワードを忘れた場合</a>
                 </div>
             </div>
         </div>
         <div class="center jumbotron p-3">
             <div class="text-center">    
-                <span>アカウントをお持ちではありませんか？</span><span><a class="color link font-weight-bold" href="{{ route('signup.get') }}">登録する</a></span>
+                <span>アカウントをお持ちではありませんか？</span><span><a class="color link font-weight-bold" href="/signup">登録する</a></span>
             </div>
         </div>
         <div class="mt-4 mb-4 text-center">
-                <span><a href="{{ route('login.guest') }}" class="link text-dark font-weight-bold"><i class="fas fa-user mr-3"></i>ゲストログイン<a></span>
+                <span><a href="{{ route('login.guest') }}" class="link text-dark font-weight-bold"><i class="fas fa-user mr-3"></i>ゲストログイン</a></span>
         </div>
     </div>
 </div>
