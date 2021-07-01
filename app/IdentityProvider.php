ae047bf9d45f5fa3aefdc06c7fdcb8fc4b26fded
ran.sakata <sakataran@outlook.jp>
2021-07-01 04:21:04 +0000

    fixed facebooklogin

--- app/IdentityProvider.php
+++ app/IdentityProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class IdentityProvider extends Model
 {
     protected $primaryKey = ['provider_name', 'provider_id'];
+    public $incrementing = false;
     protected $fillable = ['user_id', 'provider_name', 'provider_id'];
 
     function user()
